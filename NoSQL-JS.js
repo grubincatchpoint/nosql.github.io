abd191a3a843c59962291282cce53c31e1e7674e
grubincatchpoint <grubin@catchpoint.com>
2016-01-04 18:57:54 -0500

    6

--- NoSQL-JS.js
+++ NoSQL-JS.js
@@ -1,1 +1,14 @@
-#addbutton.onclick=function(){ document.getElementById(reportJSON).value += Applesauce};
+$(document).ready(function () {
+
+var jsonData = {};
+
+var repBox = document.getElementById("reportBox");
+var reportBoxName = "";
+var reportBoxID = "";
+reportBox.onclick = function(){
+var reportValue = document.getElementById("reportBox").value;
+reportBoxID = repBox.value;
+reportBoxName = repBox.text;
+};
+
+});
